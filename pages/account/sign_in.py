1b2677e6c145592482c0927af1f0be26312d03e7
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2023-11-13 01:58:13 +0300

    [+] reset password

--- pages/account/sign_in.py
+++ pages/account/sign_in.py
@@ -45,3 +45,28 @@ class SignInPage(BasePage):
 
     def create_account(self):
         return self.is_visible(self.CREATE_ACCOUNT)
+
+
+class ForgotPage(BasePage):
+    URL = "https://magento.softwaretestingboard.com/customer/account/forgotpassword/"
+    URL_DONE = "https://magento.softwaretestingboard.com/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS9jdXN0b21lci9hY2NvdW50L2luZGV4Lw%2C%2C/"
+
+    EMAIL = (By.CSS_SELECTOR, "input#email_address")
+    RESET_PASSWORD = (By.CSS_SELECTOR, "button.action.submit.primary")
+
+    SUCCESS = "If there is an account associated with %s you will receive an email with a link to reset your password."
+
+    def __init__(self, driver, url=URL):
+        super().__init__(driver, url)
+        self.current_url = url
+
+    @property
+    def email(self):
+        return self.is_visible(self.EMAIL)
+
+    @email.setter
+    def email(self, val: str):
+        self.clear_and_send_keys(self.email, val)
+
+    def reset_password(self):
+        return self.is_visible(self.RESET_PASSWORD)
--- tests/login/test_sign_in.py
+++ tests/login/test_sign_in.py
@@ -1,7 +1,7 @@
 from data.fake_data import FakeData
 from locators.login_page_locators import LoginPageLocators
 from pages.account.my_account import MyAccountPage
-from pages.account.sign_in import SignInPage
+from pages.account.sign_in import SignInPage, ForgotPage
 from pages.login.login_page import LoginPage
 from pages.login.logout_page import LogoutPage
 
@@ -22,10 +22,17 @@ class TestX(FakeData):
         page.sign_in().click()
         assert page.current_url == MyAccountPage.URL, "Login with correct credentials failed"
 
-    def test_bad_credentials_login(self, driver, email, password):
+    def test_bad_credentials_login(self, driver):
         page = SignInPage(driver)
-        page.email = email
-        page.password_one = password
+        page.email = self.email
+        page.password_one = self.password
         page.sign_in().click()
         assert page.current_url == SignInPage.URL, "Unexpected page URL"
         assert page.message_error == SignInPage.ERROR, "Successful login with random credentials"
+
+    def test_reset_password(self, driver, email):
+        page = ForgotPage(driver)
+        page.email = email
+        page.reset_password().click()
+        assert page.current_url == ForgotPage.URL_DONE, "Unexpected page URL"
+        assert page.message_success == ForgotPage.SUCCESS % email, "Message not as expected"
