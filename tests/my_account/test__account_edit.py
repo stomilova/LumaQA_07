ceca089ca6188d5df61365f4bb44d5800fbcc299
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2023-11-13 04:42:25 +0300

    [+] account add address and tests

--- tests/my_account/test__account_edit.py
+++ tests/my_account/test__account_edit.py
@@ -1,4 +1,7 @@
+from random import choice
+
 from data.fake_data import FakeData
+from pages.account.account_add_address import AddressAddPage
 from pages.account.account_edit import AccountEditPage
 from pages.account.my_account import MyAccountPage
 from pages.account.sign_in import SignInPage
@@ -49,3 +52,46 @@ class TestX(FakeData):
         page.save().click()
         assert page.current_url == SignInPage.URL, "Unexpected page URL"
         assert page.message_success == AccountEditPage.SUCCESS, "Message not as expected"
+
+    def test_add_address_with_select_state(self, driver, create_account):
+        page = AddressAddPage(driver)
+        page.company = self.company
+        page.telephone = self.phone_number
+
+        page.country = choice(AddressAddPage.WITH_REGIONS)
+        page.state = (state := choice(page.state[1:]))
+        page.city = (city := self.city)
+        page.postcode = (postcode := self.postcode)
+        page.street_1 = self.secondary_address
+        page.street_2 = self.street_address
+        page.street_3 = f"{city} {state} {postcode}"
+
+        # page.set_billing.click()
+        page.save().click()
+
+        assert page.current_url == AddressAddPage.URL_DONE
+        assert page.message_success == AddressAddPage.SUCCESS
+
+    def test_add_address_with_input_state(self, driver, create_account):
+        page = AddressAddPage(driver)
+        page.company = self.company
+        page.telephone = self.phone_number
+
+        page.country = choice(list(filter(lambda x: x not in AddressAddPage.WITH_REGIONS, page.country)))
+
+        city = self.city
+        state = self.state
+        postcode = self.postcode
+        page.street_1 = self.secondary_address
+        page.street_2 = self.street_address
+        page.street_3 = f"{city} {state} {postcode}"
+        page.city = city
+        page.postcode = postcode
+        page.region = state
+
+        # page.set_shpping.click()
+
+        page.save().click()
+
+        assert page.current_url == AddressAddPage.URL_DONE
+        assert page.message_success == AddressAddPage.SUCCESS
