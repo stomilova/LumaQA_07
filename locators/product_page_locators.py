19068074400a015b1ca78956f80003aa3553e1af
Nick Tikhomirov <la8185324310@gmail.com>
2023-11-14 13:30:49 -0800

    add: product_page_locators.py

---
+++ locators/product_page_locators.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.common.by import By
+
+class ProductPageLocators:
+
+    # class="product-info-main"
+    PRODUCT_NAME = (By.XPATH, "//span[@itemprop='name']")
+
+    # class="product media"
+
+    # class="product info detailed"
+
+
+    # class="block upsell"
+
