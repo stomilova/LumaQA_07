4ae28815a5061c063d779741224431a2813d4b32
teemax88 <redline_pr@mail.ru>
2023-11-15 02:23:30 +0500

    Сборка теста: Оформление заказа (позитивный)

--- conftest.py
+++ conftest.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 def options():
     options = Options()
     options.add_argument('--window-size=2880,1800')
-    options.add_argument('--headless')
+    # options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     return options 
