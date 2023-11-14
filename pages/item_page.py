cc1b93cd48db2d3c6c3273cc8c2d32d9e6f7f34b
danya-f <danya-f@mail.ru>
2023-11-14 23:10:49 +0300

    add : more item url's and item_reviews methods

--- pages/item_page.py
+++ pages/item_page.py
@@ -1,3 +1,7 @@
+import math
+
+from selenium.webdriver.common.by import By
+
 from base.seleniumbase import BasePage
 from locators.base_page_locators import BasePageLocators
 from locators.item_page_locators import ItemPageLocators
@@ -5,6 +9,11 @@ from locators.item_page_locators import ItemPageLocators
 
 class ItemPage(BasePage):
     URL_DRIVEN_BACKPACK = "https://magento.softwaretestingboard.com/driven-backpack.html"
+    URL_PUSH_IT_MESSENGER_BAG = "https://magento.softwaretestingboard.com/push-it-messenger-bag.html"
+    URL_HARMONY_LUMAFLEX_STRENGTH_BAND_KIT = "https://magento.softwaretestingboard.com/harmony-lumaflex-trade-strength-band-kit.html"
+    URL_PURSUIT_LUMAFLEX_TONE_BAND = "https://magento.softwaretestingboard.com/pursuit-lumaflex-trade-tone-band.html"
+    URL_LUCIA_CROSS_FIT_BRA = "https://magento.softwaretestingboard.com/lucia-cross-fit-bra.html"
+    URL_HERA_PULLOVER_HOODIE = "https://magento.softwaretestingboard.com/hera-pullover-hoodie.html"
 
     def enter_quantity_items(self, quantity):
         self.is_clickable(ItemPageLocators.QUANTITY_OF_ITEM).clear()
@@ -15,3 +24,32 @@ class ItemPage(BasePage):
         self.enter_quantity_items(quantity)
         self.is_clickable(ItemPageLocators.ADD_TO_CART_BUTTON).click()
         self.is_visible(BasePageLocators.MSG_SUCCESS)
+
+    def item_review_count(self):
+        return int(self.is_visible(ItemPageLocators.ITEM_REVIEW_COUNT).text)
+
+    def item_review(self):
+        return self.is_clickable(ItemPageLocators.ITEM_REVIEW_LINK)
+
+    def customer_review_header(self):
+        return self.is_visible(ItemPageLocators.CUSTOMER_REVIEWS_HEADER)
+
+    def item_rating(self):
+        return int(self.is_visible(ItemPageLocators.ITEM_RATING).get_attribute('title').split('%')[0])
+
+    def add_your_review_link(self):
+        return self.is_clickable(ItemPageLocators.ADD_YOUR_REVIEW_LINK)
+
+    def block_review_add(self):
+        return self.is_visible(ItemPageLocators.BLOCK_REVIEW_ADD)
+
+    def get_customer_review(self, number_of_review):
+        return self.is_visible((By.XPATH,
+                                f"(//div[@class = 'review-ratings']//div[@class='rating-result'])[{number_of_review}]")).get_attribute(
+            'title')
+
+    def get_overall_rating(self, count):
+        overall_rating = 0
+        for i in range(1, count + 1):
+            overall_rating += int((self.get_customer_review(i).split('%'))[0])
+        return math.ceil(overall_rating / count)
