70affcd6d4f3bbcba80acfde2f4642e0305abef2
Aleksei Tumakov <109537858+gomboc665@users.noreply.github.com>
2023-11-15 20:28:24 +0400

    Update test_urls_list.py
    [-] the list of URLs for Footer tests

--- data/test_urls_list.py
+++ data/test_urls_list.py
@@ -1,19 +1,18 @@
-
-# может быть использовано всеми, кому нужно сделать тесты на наличие элемента на каждой странице сайта, документ может быть переименован по усмотрению,
-# и дополнен большим колличеством страницу
+# List for Footer test
 TEST_URL_LIST = [
     "https://magento.softwaretestingboard.com/",
     "https://magento.softwaretestingboard.com/what-is-new.html",
-    "https://magento.softwaretestingboard.com/women.html",
-    "https://magento.softwaretestingboard.com/men.html",
-    "https://magento.softwaretestingboard.com/gear.html",
-    "https://magento.softwaretestingboard.com/training.html",
-    "https://magento.softwaretestingboard.com/sale.html",
-    "https://magento.softwaretestingboard.com/gear/bags.html",
-    "https://magento.softwaretestingboard.com/gear/fitness-equipment.html",
-    "https://magento.softwaretestingboard.com/gear/watches.html",
-    "https://magento.softwaretestingboard.com/men/tops-men.html",
-    "https://magento.softwaretestingboard.com/men/bottoms-men.html",
-    "https://magento.softwaretestingboard.com/women/tops-women.html",
-    "https://magento.softwaretestingboard.com/women/bottoms-women.html",
 ]
+#     "https://magento.softwaretestingboard.com/women.html",
+#     "https://magento.softwaretestingboard.com/men.html",
+#     "https://magento.softwaretestingboard.com/gear.html",
+#     "https://magento.softwaretestingboard.com/training.html",
+#     "https://magento.softwaretestingboard.com/sale.html",
+#     "https://magento.softwaretestingboard.com/gear/bags.html",
+#     "https://magento.softwaretestingboard.com/gear/fitness-equipment.html",
+#     "https://magento.softwaretestingboard.com/gear/watches.html",
+#     "https://magento.softwaretestingboard.com/men/tops-men.html",
+#     "https://magento.softwaretestingboard.com/men/bottoms-men.html",
+#     "https://magento.softwaretestingboard.com/women/tops-women.html",
+#     "https://magento.softwaretestingboard.com/women/bottoms-women.html",
+#]
