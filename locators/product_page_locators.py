743f795feb43a30aea500260adfd2830c4c14f94
Nick Tikhomirov <la8185324310@gmail.com>
2023-11-14 16:59:14 -0800

    add: locators into ProductPageLocators

--- locators/product_page_locators.py
+++ locators/product_page_locators.py
@@ -2,6 +2,14 @@ from selenium.webdriver.common.by import By
 
 class ProductPageLocators:
 
+    # Product page
+    HEADER = (By.XPATH, "//header[@class='page-header']")
+    NAVIGATION_SECTION = (By.XPATH, "//div[@class='sections nav-sections']")
+    BREADCRUMBS = (By.XPATH, "//div[@class='breadcrumbs']")
+    BODY = (By.XPATH, "//main[@id='maincontent']")
+    FOOTER = (By.XPATH, "//footer[@class='page-footer']")
+    COPYRIGHT = (By.XPATH, "//small[@class='copyright']")
+
     # class="product-info-main"
     PRODUCT_NAME = (By.XPATH, "//span[@itemprop='name']")
 
