1afa227af35de9f4ee14a35d3ce81dd4e94a5b5c
Nick Tikhomirov <la8185324310@gmail.com>
2023-11-14 17:02:36 -0800

    add: locators into ProductPageLocators

---
+++ tests/product_page/test_product_page_structure.py
@@ -0,0 +1,18 @@
+import pytest
+from data.product_page_data import PRODUCT_PAGE_URL_LIST, TIMEOUT
+from locators.product_page_locators import ProductPageLocators
+from pages.product_page.product_page import ProductPage
+
+@pytest.mark.parametrize("url", PRODUCT_PAGE_URL_LIST)
+def test_product_name_in_page_title(driver, url):
+    """
+    TC_002.002.001
+    """
+    product_page = ProductPage(driver, url)
+    product_page.open()
+    assert product_page.is_visible(ProductPageLocators.HEADER, TIMEOUT)
+    assert product_page.is_visible(ProductPageLocators.NAVIGATION_SECTION, TIMEOUT)
+    assert product_page.is_visible(ProductPageLocators.BREADCRUMBS, TIMEOUT)
+    assert product_page.is_visible(ProductPageLocators.BODY, TIMEOUT)
+    assert product_page.is_visible(ProductPageLocators.FOOTER, TIMEOUT)
+    assert product_page.is_visible(ProductPageLocators.COPYRIGHT, TIMEOUT)
