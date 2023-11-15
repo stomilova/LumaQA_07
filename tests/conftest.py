6022bd9e082954b8ac7bbf73e7627c28b4995176
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2023-11-15 11:53:03 +0300

    [+] auto screenshot on failure

--- tests/conftest.py
+++ tests/conftest.py
@@ -28,3 +28,18 @@ def password():
 @pytest.fixture
 def create_account(driver):
     CreateAccountPage(driver)
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+    setattr(item, "rep_" + rep.when, rep)
+
+
+@pytest.fixture(autouse=True)
+def check_if_failed(request, driver):
+    yield
+    if request.node.rep_setup.passed and request.node.rep_call.failed:
+        fn = f'{request.node.nodeid}_{strftime("%H_%M")}.png'.replace("/", "-").replace(":", "_")
+        driver.save_screenshot(fn)
+        driver.quit()
