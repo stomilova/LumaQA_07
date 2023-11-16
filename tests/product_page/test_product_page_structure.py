daa58093b8cd28c56e24ef9b741516bc553f3ebd
Nick Tikhomirov <la8185324310@gmail.com>
2023-11-15 19:48:11 -0800

    Refactor: fix test description

--- tests/product_page/test_product_page_structure.py
+++ tests/product_page/test_product_page_structure.py
@@ -6,7 +6,23 @@ from pages.product_page.product_page import ProductPage
 @pytest.mark.parametrize("url", PRODUCT_PAGE_URL_LIST)
 def test_product_page_structure(driver, url):
     """
-    TC_002.002.001
+    TC_002.002.002 | Product page > DOM > Product page structure
+    
+    Precondition:
+    A guest user on any product page (link as example).
+    
+    Steps:
+    Verify that product page contains next elements:
+    1.	header class="page-header"
+    2.	div class="sections nav-sections"
+    3.	div class="breadcrumbs"
+    4.	main class=" maincontent"
+    5.	footer class="page-footer"
+    6.	small class="copyright"
+    
+    Expected result:
+    Any product page DOM contains all listed elements.
+
     """
     product_page = ProductPage(driver, url)
     product_page.open()
