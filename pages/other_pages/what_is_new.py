59122c3a421f3b63575e28283732fc7fa0216057
lion7500000 <55666766+lion7500000@users.noreply.github.com>
2023-11-15 19:37:08 -0800

    TC _006.003.003 (#43)

--- pages/other_pages/what_is_new.py
+++ pages/other_pages/what_is_new.py
@@ -6,9 +6,11 @@ class NewPage(BasePage):
 
     TITLE = (By.CSS_SELECTOR, 'span[data-ui-id="page-title-wrapper"]')
     YOGA_COLL = (By.CSS_SELECTOR, 'a.new-main  span.info')
+    YOGA_SUBTITLE = (By.CSS_SELECTOR, 'a.new-main  strong.title')
 
     TITLE_TEXT = "What's New"
     YOGA_WIDGET_TITLE = "New Luma Yoga Collection"
+    YOGA_SUBTITLE_TEXT = 'The very latest yoga styles plus twists on timeless classics'
 
 
     def __init__(self, driver, url=URL):
@@ -20,4 +22,7 @@ class NewPage(BasePage):
 
     def widget_yoga_call(self) -> str:
         return self.is_visible(self.YOGA_COLL).text
+
+    def widget_subtitle(self) -> str:
+        return self.is_visible(self.YOGA_SUBTITLE).text
 
--- tests/what_is_new_page/test_what_is_new_page.py
+++ tests/what_is_new_page/test_what_is_new_page.py
@@ -15,3 +15,10 @@ def test_widget_yoga(driver):
     widget_title = page.YOGA_WIDGET_TITLE
     assert widget_title == NewPage.YOGA_WIDGET_TITLE, (f'Expected text: {NewPage.YOGA_WIDGET_TITLE}, but got: '
                                                        f'{widget_title}')
+
+def test_widget_subtitle(driver):
+    page = NewPage(driver, url=NewPage.URL)
+    page.open()
+    subtitle = page.widget_subtitle()
+    assert subtitle == NewPage.YOGA_SUBTITLE_TEXT, f'Expected text: {NewPage.YOGA_SUBTITLE_TEXT}, but got: {subtitle}'
+
