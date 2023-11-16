ca3302cd17b6f1e9f8ee18accc9a634b1a03eae6
Nick Tikhomirov <la8185324310@gmail.com>
2023-11-15 19:41:42 -0800

    Refactor: fix test description

--- tests/product_page/test_product_name_in_page_title.py
+++ tests/product_page/test_product_name_in_page_title.py
@@ -6,7 +6,13 @@ from pages.product_page.product_page import ProductPage
 @pytest.mark.parametrize("url", PRODUCT_PAGE_URL_LIST)
 def test_product_name_in_page_title(driver, url):
     """
-    TC_002.001.001
+    TC_002.001.003 | Product page > Title > Product name in the page title
+    Precondition:
+    A guest user on any product page (link as example).
+    Steps:
+    1.	Verify that current product name is displayed on the page title.
+    Expected result:
+    Current product name is displayed in the page title.
     """
     product_page = ProductPage(driver, url)
     product_page.open()
