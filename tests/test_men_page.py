96e45877fdca6a83a335ebde92164eb5e2cc3386
vvdanilovby@gmail.com <62068685+valdemards@users.noreply.github.com>
2023-11-16 12:51:53 +0400

    TC_004.001.001 | Authorization >Anonym user > Accessibility of site pages > Menu sections

--- tests/test_men_page.py
+++ tests/test_men_page.py
@@ -1,7 +1,7 @@
 import time
 
 from pages.men_page import MenPage
-from data.men_page_url import MEN_PAGE, TOPS_MEN_PAGE, BOTTOMS_MEN_PAGE, JACKETS_MEN_PAGE
+from data.men_page_url import MEN_PAGE, TOPS_MEN_PAGE, MEN_BOTTOMS_PAGE, MEN_TOPS_JACKETS_PAGE
 
 
 class TestMenPage:
@@ -22,11 +22,11 @@ class TestMenPage:
         page.open()
         page.select_tops_from_sidebar_menu()
         page.select_jackets_from_tops_dropdown()
-        assert driver.current_url == JACKETS_MEN_PAGE
+        assert driver.current_url == MEN_TOPS_JACKETS_PAGE
 
     def test_redirect_to_bottoms_men_page_from_men_dropdown_btn(self, driver):
         page = MenPage(driver, MEN_PAGE)
         page.open()
         page.select_bottoms_from_men_dropdown()
-        assert driver.current_url == BOTTOMS_MEN_PAGE
+        assert driver.current_url == MEN_BOTTOMS_PAGE
 
