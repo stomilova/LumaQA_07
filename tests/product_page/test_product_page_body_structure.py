cc241f3d8d412d658deaf88003b465331119c212
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2023-11-17 16:42:02 +0300

    [!] так лучше

--- tests/product_page/test_product_page_body_structure.py
+++ tests/product_page/test_product_page_body_structure.py
@@ -1,7 +1,10 @@
 import pytest
+from selenium.common import TimeoutException
+
 from data.product_page_data import PRODUCT_PAGE_URL_LIST, TIMEOUT
 from locators.product_page_locators import ProductPageLocators
 from pages.product_page.product_page import ProductPage
+
 
 @pytest.mark.parametrize("url", PRODUCT_PAGE_URL_LIST)
 def test_product_page_body_structure(driver, url):
@@ -30,12 +33,17 @@ def test_product_page_body_structure(driver, url):
 
     # Since I didn't find didn't find the distribution logic with related and liked products,
     # I just check if one of them exists
+    related_products_visible = None
+    liked_products_visible = None
     try:
         related_products_visible = product_page.is_visible(ProductPageLocators.RELATED_PRODUCTS)
+    except TimeoutException:
+        pass
+    try:
         liked_products_visible = product_page.is_visible(ProductPageLocators.LIKED_PRODUCTS)
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    else:
-        assert related_products_visible or liked_products_visible
+    except TimeoutException:
+        pass
+
+    assert related_products_visible or liked_products_visible
 
 
