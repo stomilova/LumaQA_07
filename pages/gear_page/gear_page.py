dfff17fd4ffe190d7d2f8f0d0e8eb78c4a751751
gomboc665 <slowriotprint@gmail.com>
2023-11-17 12:37:57 +0400

    removing spaces, adding additional method(all for gear page)

--- pages/gear_page/gear_page.py
+++ pages/gear_page/gear_page.py
@@ -1,13 +1,8 @@
-from selenium.webdriver.common.by import By
 from base.seleniumbase import BasePage
 from data.gear_page_urls import GEAR_PAGE
 
 
 class GearPage(BasePage):
     # page with categories
-
     def __init__(self, driver):
         super().__init__(url=GEAR_PAGE,driver=driver)
-
-
-   
