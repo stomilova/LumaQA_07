b2f8f2e9d02b05bbd7af33d14fcb0285aab013b0
marinapauli <marishka81@gmail.com>
2023-11-17 11:47:24 -0800

    Added test case number to the tests

--- tests/product_page/test_product_main_info.py
+++ tests/product_page/test_product_main_info.py
@@ -4,12 +4,14 @@ from data.product_page_data import PRODUCT_PAGE_EXAMPLE
 class TestProductPage:
 
     def test_check_product_name_in_main_info(self, driver):
+        """TC_002.005.001"""
         page = ProductPage(driver, PRODUCT_PAGE_EXAMPLE)
         page.open()
         text = page.check_product_name_in_main_info()
         assert text == "Breathe-Easy Tank"
 
     def test_rating_block_is_visible(self, driver):
+        """TC_002.005.002"""
         page = ProductPage(driver, PRODUCT_PAGE_EXAMPLE)
         page.open()
         assert page.rating_block_is_visible()
