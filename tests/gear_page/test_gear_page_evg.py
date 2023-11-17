2a633103f82cd726fae2574ddeb9851830d79a74
evg-QA <freecadelka@yahoo.com>
2023-11-17 19:06:49 +0300

    TC_009.003.002 | added: test_loosen_up_is_visible

--- tests/gear_page/test_gear_page_evg.py
+++ tests/gear_page/test_gear_page_evg.py
@@ -7,3 +7,9 @@ def test_sprite_yoga_companion_kit_is_visible(driver):
     page = BasePage(driver, url=GEAR_PAGE)
     page.open()
     assert page.is_visible(BannerLocators.SPRITE_YOGA_COMPANION_KIT_BANNER)
+
+def test_loosen_up_is_visible(driver):
+    """TC_009.003.002 | Gear page > categories > Visibility of the 'Loosen Up' banner"""
+    page = BasePage(driver, url=GEAR_PAGE)
+    page.open()
+    assert page.is_visible(BannerLocators.LOOSEN_UP_BANNER)
