c57ef46f9128e487df607e49f995d210721c9ed8
Aleksandr Matveev <alrmatveev12@gmail.com>
2023-11-18 08:01:18 +0100

    added URL

--- data/men_page_url.py
+++ data/men_page_url.py
@@ -7,3 +7,6 @@ MEN_TOPS_TANKS_PAGE = 'https://magento.softwaretestingboard.com/men/tops-men/tan
 MEN_BOTTOMS_PAGE = 'https://magento.softwaretestingboard.com/men/bottoms-men.html'
 MEN_BOTTOMS_PANTS_PAGE = 'https://magento.softwaretestingboard.com/men/bottoms-men/pants-men.html'
 MEN_BOTTOMS_SHORTS_PAGE = 'https://magento.softwaretestingboard.com/men/bottoms-men/shorts-men.html'
+
+# URL with selected filters
+MEN_HOODIES_AND_SWEATSHIRTS_FILTER = 'https://magento.softwaretestingboard.com/men/tops-men.html?cat=15'
