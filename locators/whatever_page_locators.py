5849309841b378a93c51a7762c10a9ed68dececc
OlenaKS <okosaeva@yandex.ru>
2023-11-18 11:24:13 +0300

    Changes TC 006.004.002

--- locators/whatever_page_locators.py
+++ locators/whatever_page_locators.py
@@ -2,5 +2,6 @@ from selenium.webdriver.common.by import By
 
 
 class WhateverPageLocators:
-    Whatever_Day = (By.XPATH, "//*[text()='Whatever day brings']")
-    Perfomance_Fabrics = (By.XPATH, "//*[text()='Performance Fabrics']")
+    Whatever_Day = (By.CSS_SELECTOR,'a[class="block-promo new-performance"] > span > strong')
+    Perfomance_Fabrics = (By.CSS_SELECTOR,'a[class="block-promo new-performance"] > span > span[class="more icon"]')
+
--- tests/test_whatever_widget.py
+++ tests/test_whatever_widget.py
@@ -7,6 +7,7 @@ def test_whatever_widget(driver):
     page = WhateverPage(driver, "https://magento.softwaretestingboard.com/what-is-new.html")
     page.open()
     page.find_widget_whatever_day()
+    assert 'Whatever day brings' in driver.page_source
     page.find_widget_perfomance_fabrics()
-
     assert 'Performance Sportswear New' in driver.page_source
+
