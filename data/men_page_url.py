9f02cd0867cebefad98d9bc845649a13c1571a8b
Aleksandr Matveev <alrmatveev12@gmail.com>
2023-11-18 15:06:15 +0100

    added URL

--- data/men_page_url.py
+++ data/men_page_url.py
@@ -10,3 +10,6 @@ MEN_BOTTOMS_SHORTS_PAGE = 'https://magento.softwaretestingboard.com/men/bottoms-
 
 # URL with selected filters
 MEN_HOODIES_AND_SWEATSHIRTS_FILTER = 'https://magento.softwaretestingboard.com/men/tops-men.html?cat=15'
+
+# Product pages
+MARCO_LIGHTWEIGHT = 'https://magento.softwaretestingboard.com/marco-lightweight-active-hoodie.html'
