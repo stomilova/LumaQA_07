263b0a08e96d71afd6eb92145b664c322a1f185a
Vadim Trefilov <90708778+VadimTref@users.noreply.github.com>
2023-11-18 20:16:15 -0500

    TC_015.001.005 | Popular Search Terms > Content > Verify among the keywords with the enlarged font size are defined words (#105)

--- tests/popular_search_terms/test_content_popular_search_terms_page.py
+++ tests/popular_search_terms/test_content_popular_search_terms_page.py
@@ -4,6 +4,10 @@ from data.popular_search_terms_page_data import POPULAR_SEARCH_TERMS_PAGE_URL, H
 
 class TestContentPopularSearchTermsPage:
 
+    @staticmethod
+    def get_keyword_font_size(keyword):
+        return float(keyword.get_attribute('style').split(':')[-1].replace('%;', ''))
+
     def test_verify_heading_on_popular_search_terms_page(self, driver):
         page = PopularSearchTermsPage(driver, POPULAR_SEARCH_TERMS_PAGE_URL)
         page.open()
@@ -21,7 +25,22 @@ class TestContentPopularSearchTermsPage:
         page = PopularSearchTermsPage(driver, POPULAR_SEARCH_TERMS_PAGE_URL)
         page.open()
 
-        keywords_filtered_list = [keyword for keyword in page.get_keywords_list()
-                                  if float(keyword.get_attribute('style').split(':')[-1].replace('%;', '')) > 88]
+        keywords_filtered_list = [keyword for keyword in page.get_keywords_list() if self.get_keyword_font_size(keyword) > 88]
 
         assert len(keywords_filtered_list) >= 5
+
+    def test_verify_among_keywords_with_enlarged_font_size_are_defined_words(self, driver):
+        page = PopularSearchTermsPage(driver, POPULAR_SEARCH_TERMS_PAGE_URL)
+        page.open()
+
+        keywords_dict = {keyword.text: self.get_keyword_font_size(keyword) for keyword in page.get_keywords_list()}
+
+        keywords_filtered_list = []
+        i = 1
+        while i <= 10:
+            keyword_name = [key for (key, value) in keywords_dict.items() if value == max(keywords_dict.values())][0]
+            keywords_filtered_list.append(keyword_name)
+            del keywords_dict[keyword_name]
+            i += 1
+
+        assert {'bag', 'HOODIE', 'jacket', 'pants', 'shirt'}.issubset(keywords_filtered_list)
