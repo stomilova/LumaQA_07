eefcf64fc56537a96f8d40b9dd3be2725103d61a
OlenaKS <144878056+OlenaKS@users.noreply.github.com>
2023-11-21 17:01:10 +0300

    ADD TC 006.006.001 (#139)
    * ADD TC 006.006.001
    
    * Update seleniumbase.py
    
    ---------
    
    Co-authored-by: aqaestuser <144237758+aqaestuser@users.noreply.github.com>

---
+++ pages/other_pages/lumas_latest.py
@@ -0,0 +1,36 @@
+from selenium.webdriver import ActionChains
+
+from locators.lumas_latest_page_locators import LumasPageLocators
+from base.seleniumbase import BasePage
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class LumasPage(BasePage):
+    def find_lumas(self):
+        return self.is_visible(LumasPageLocators.Lumas_Latest)
+
+    def find_lumas_just(self):
+
+        return self.is_visible(LumasPageLocators.Just_in_Time).text
+
+    def find_elements_lumas(self):
+
+        item_list = self.is_visible_all_elements(LumasPageLocators.Item_List)
+        for item in item_list:
+            print(item.text)
+
+    def hold_mou_on_elementse_img(self):
+        ActionChains(self.driver).move_to_element(self.is_visible(LumasPageLocators.IMG)).perform()
+    def find_add_button_cart(self):
+         return self.is_visible(LumasPageLocators.Add_to_Cart).text
+    def find_add_wish(self):
+
+         return self.is_visible(LumasPageLocators.Image_of_Wish).text
+    def find_add_compare(self):
+        return self.is_visible(LumasPageLocators.Image_of_Compare).text
+
+
+
+
+
