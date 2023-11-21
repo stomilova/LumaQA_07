1ae9e22ce0d58588c92e3fc8b4e2474a5fa2753d
S-Lanchik <130118964+S-Lanchik@users.noreply.github.com>
2023-11-21 19:54:58 +0300

    TC_008.005.002 | Tops page > Products >Verify a mode-switcher greed is Clickable (#145)

--- locators/men_page_locators.py
+++ locators/men_page_locators.py
@@ -8,7 +8,7 @@ class MenPageLocators:
     TOPS_CATEGORY_LINK = (By.XPATH, "//*[@id='narrow-by-list2']/dd/ol/li[1]/a")
     BOTTOMS_CATEGORY_LINK = (By.XPATH, "//*[@id='narrow-by-list2']/dd/ol/li[2]/a")
     SIDE_BAR_JACKETS = (By.XPATH, "//a[@id='ui-id-19']//span[contains(text(),'Jackets')]")
-
+    MEN_TOPS_GRID = (By.XPATH, "//div[2]/div[1]/strong[2]")
 
 class MenCategoryPageLocators:
     CATEGORY_BUTTON = (
