f761b828fd195d1b343d547d8b96bc049db0e3d9
evg-QA <freecadelka@yahoo.com>
2023-11-22 14:50:25 +0300

    TC_012.007.004 | added: test_text_block_for_typos_titled_your_choices_regarding_use_of_the_information_we_collect

--- tests/privacy_and_cookie_policy_page/test_privacy_and_cookie_policy.py
+++ tests/privacy_and_cookie_policy_page/test_privacy_and_cookie_policy.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from data.privacy_and_cookie_policy_page_urls import PRIVACY_AND_COOKIE_POLICY_PAGE
 from data.privacy_and_cookie_policy_page_fonts import PrivacyCookiePolicyFonts
 from locators.privacy_and_cookie_policy_page_locators import PrivacyCookiePolicyPageLocators
+import language_tool_python
 
 def test_text_block_font_family_titled_cour_choices_regarding_use_of_the_information_we_collect(driver):
     """TC_012.007.001 | Footer > "Privacy and Cookie Policy" > Content >
@@ -30,3 +31,17 @@ def test_text_block_font_size_titled_cour_choices_regarding_use_of_the_informati
     element = driver.find_element(By.XPATH, PrivacyCookiePolicyPageLocators.BLOCK_CONTENT_LOCATOR)
     font_size = element.value_of_css_property('font-size')
     assert font_size == PrivacyCookiePolicyFonts.TEXT_FONT_SIZE
+
+def test_text_block_for_typos_titled_your_choices_regarding_use_of_the_information_we_collect(driver):
+    """TC_012.007.004 | Footer > "Privacy and Cookie Policy" > Content > Verify the text block and title for typos
+    titled ‘Your Choices Regarding Use Of The Information We Collect’"""
+    page = BasePage(driver, url=PRIVACY_AND_COOKIE_POLICY_PAGE)
+    page.open()
+    text_header = driver.find_element(By.XPATH, PrivacyCookiePolicyPageLocators.BLOCK_CONTENT_HEADER_LOCATOR).text
+    text_block = driver.find_element(By.XPATH, PrivacyCookiePolicyPageLocators.BLOCK_CONTENT_LOCATOR).text
+    tool = language_tool_python.LanguageTool('en-US')
+    matches_header = tool.check(text_header)
+    assert len(matches_header) == 0, f"Grammar mistakes have been found in the header: {matches_header}"
+    matches_block = tool.check(text_block)
+    assert len(matches_block) == 0, f"Grammar mistakes have been found in the text block: {matches_block}"
+
