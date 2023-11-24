658726c77a4227b1ebad0b4ea7a0b58b89f9c7d0
TanyushaLutsenko <97060725+TanyushaLutsenko@users.noreply.github.com>
2023-11-24 01:30:29 -0400

    Add test_main_logo_redirect_to_main_page for TC_003.001.002 (#170)
    Co-authored-by: aqaestuser <144237758+aqaestuser@users.noreply.github.com>

--- tests/product_page/test_product_main_info.py
+++ tests/product_page/test_product_main_info.py
@@ -1,10 +1,12 @@
 import locators.product_page_locators
 from pages.product_page.product_main_info import ProductPage
 from data.product_page_data import PRODUCT_PAGE_EXAMPLE
+from locators.product_page_locators import ProductPageLocators
+from locators.base_page_locators import BasePageLocators as BP
+from data.home_page_url import HOME_PAGE
 from pages.account.sign_in import SignInPage
 from locators.product_page_locators import ProductPageLocators as loc
 from tests.login.test__sign_in import TestX
-import time
 
 
 class TestProductPage:
@@ -55,6 +57,14 @@ class TestProductPage:
         TestX.test_correct_credentials_login(self, driver)
         page = ProductPage(driver, PRODUCT_PAGE_EXAMPLE)
         page.open()
+        assert page.visible(ProductPageLocators.ADD_WISH_ELEMENT), "Element 'Add to wish list' is invisible"
+
+    def test_main_logo_redirect_to_main_page(self, driver):
+        """TC_003.001.002"""
+        page = ProductPage(driver, PRODUCT_PAGE_EXAMPLE)
+        page.open()
+        page.visible(BP.LOGO_TITLE).click()
+        assert driver.current_url == HOME_PAGE
         assert page.visible(loc.ADD_WISH_ELEMENT), "Element 'Add to wish list' is invisible"
 
     def test_add_to_wish_list_is_clickable(self, driver):
@@ -64,3 +74,4 @@ class TestProductPage:
         page.open()
         page.visible(loc.ADD_WISH_ELEMENT).click()
         assert driver.find_element(*loc.NAME_OF_WISH_LIST).text == "My Wish List"
+
