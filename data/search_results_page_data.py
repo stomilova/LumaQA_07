e015db18fe37563fc397e4703f1c71e087273f00
Vadim Trefilov <90708778+VadimTref@users.noreply.github.com>
2023-11-23 19:58:44 -0500

    TC_015.003.002 | Popular Search Terms > Redirection > Verify the title of the new page upon clicking on the appropriate keyword link (#167)

--- data/search_results_page_data.py
+++ data/search_results_page_data.py
@@ -1,1 +1,2 @@
 HOODIE_ITEM_SEARCH_RESULT_URL = 'https://magento.softwaretestingboard.com/catalogsearch/result/?q=HOODIE'
+HOODIE_ITEM_SEARCH_RESULT_TITLE = "Search results for: 'HOODIE'"
--- tests/popular_search_terms/test_redirection_upon_click_on_keyword_links.py
+++ tests/popular_search_terms/test_redirection_upon_click_on_keyword_links.py
@@ -1,14 +1,22 @@
 from pages.popular_search_terms.popular_search_terms_page import PopularSearchTermsPage
 from data.popular_search_terms_page_data import POPULAR_SEARCH_TERMS_PAGE_URL
-from data.search_results_page_data import HOODIE_ITEM_SEARCH_RESULT_URL
+from data.search_results_page_data import HOODIE_ITEM_SEARCH_RESULT_URL, HOODIE_ITEM_SEARCH_RESULT_TITLE
 
 
 class TestRedirectionUponClickOnKeywordLinks:
 
-    def test_verify_redirection_to_the_correct_page(self,driver):
+    def test_verify_redirection_to_the_correct_page(self, driver):
         page = PopularSearchTermsPage(driver, POPULAR_SEARCH_TERMS_PAGE_URL)
         page.open()
 
         page.click_on_hoodie_item_keyword_link()
 
         assert page.current_url == HOODIE_ITEM_SEARCH_RESULT_URL
+
+    def test_verify_redirection_to_the_page_with_correct_title(self, driver):
+        page = PopularSearchTermsPage(driver, POPULAR_SEARCH_TERMS_PAGE_URL)
+        page.open()
+
+        page.click_on_hoodie_item_keyword_link()
+
+        assert driver.title == HOODIE_ITEM_SEARCH_RESULT_TITLE
