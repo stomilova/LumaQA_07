53a067418b3f4f7dbd0615e73e35283924661321
verafes <verafes@gmail.com>
2023-11-24 18:34:53 -0800

    TC_012.012.002

--- tests/footer/test_footer.py
+++ tests/footer/test_footer.py
@@ -13,6 +13,19 @@ def footer_page(driver):
 
 class TestFooterPage:
     def test_check_visibility_advanced_search_link(self, driver, footer_page):
-        """TC_012.012.001 | Footer > > Advanced search link > Visibility"""
+        """
+        TC_012.012.001 | Footer > Advanced search link > Visibility
+        The test verifies if the advanced search link is visible on the footer
+        """
         assert footer_page.check_visibility_advanced_search_link(), \
             "Advanced Search link is not visible"
+
+    def test_check_clickability_advanced_search_link(self, driver, footer_page):
+        """
+        TC_012.012.002 | Footer > Advanced search link > Clickability
+        The test verifies if the advanced search link is clickable
+        and redirects to the Advanced Search page
+        """
+        footer_page.click_advanced_search_link()
+        assert "advanced" in driver.current_url.lower(), \
+            "Advanced Search link is not clickable or doesn't redirect to the Advanced Search page"
