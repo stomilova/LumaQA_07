3e92f414bd6fca0113ab742764044d02db730994
evg-QA <freecadelka@yahoo.com>
2023-11-27 12:19:38 +0300

    TC_012.003.002 | added: class PopularSearchTermsPageFooterLocators -> FOOTER_SEARCH_TERMS_LINK

--- locators/popular_search_terms_page_locators.py
+++ locators/popular_search_terms_page_locators.py
@@ -1,7 +1,10 @@
 from selenium.webdriver.common.by import By
 
-
 class PopularSearchTermsPageLocators:
     HEADING = (By.CSS_SELECTOR, '.base')
     KEYWORDS_LIST = (By.CSS_SELECTOR, '[class="item"] > a')
     HOODIE_ITEM_KEYWORDS_LINK = (By.CSS_SELECTOR, '[href$="HOODIE"]')
+
+class PopularSearchTermsPageFooterLocators:
+    FOOTER_SEARCH_TERMS_LINK = (By.XPATH, "//li[@class='nav item current']")
+
