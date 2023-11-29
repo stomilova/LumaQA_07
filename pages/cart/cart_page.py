22331e265a5f9fb17f434ad3b6f83f0c989f4412
SviatlanaKot <145499431+SviatlanaKot@users.noreply.github.com>
2023-11-29 14:51:51 +0300

    Edit item (#221)
    * "TC_008.009.002" update
    
    * TC_005.002.006, Check ability to edit shopping cart

--- pages/cart/cart_page.py
+++ pages/cart/cart_page.py
@@ -22,6 +22,36 @@ class CartPage(BasePage):
         self.wait_shipping_addresses_overlay_closed()
         self.wait_shipping_methods_overlay_closed()
 
+    def button_edit_item(self):
+        return self.is_clickable(CartPageLocators.BUTTON_EDIT_ITEM)
+
+    def item_options(self):
+        return self.is_visible(CartPageLocators.ITEM_OPTIONS)
+
+
+class UpdateItem(BasePage):
+
+    def size_29(self):
+        return self.is_clickable(CartPageLocators.SIZE_UPDATE_29)
+
+    def color_blue(self):
+        return self.is_clickable(CartPageLocators.COLOR_BLUE)
+
+    def update_cart(self):
+        return self.is_clickable(CartPageLocators.BUTTON_UPDATE_CART)
+
+
+class AddItemToCart(BasePage):
+    def add_to_cart(self):
+        return self.is_clickable(CartPageLocators.BUTTON_ADD_TO_CART)
+
+    def color_grey_item(self):
+        return self.is_clickable(CartPageLocators.COLOR_GREY)
+
+    def size_28(self):
+        return self.is_clickable(CartPageLocators.SIZE_28)
+
+
 
 
 
