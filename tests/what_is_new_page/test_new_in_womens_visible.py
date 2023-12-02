507d243df86f4e67561aca03a3a1131ed149c252
aglebkina <121031731+aglebkina@users.noreply.github.com>
2023-12-02 15:34:59 +0530

    changes after review tc 006.001.001

--- tests/what_is_new_page/test_new_in_womens_visible.py
+++ tests/what_is_new_page/test_new_in_womens_visible.py
@@ -5,41 +5,41 @@ from locators.whats_new_page_locators import NewInWomenLocators
 
 def test_headling_visible(driver):
     driver.get(NewPage.URL)
-    find_headling = driver.find_element(*NewInWomenLocators.new_in_women_title)
-    assert find_headling.text == NewInWomenLocators.new_in_women_title_text, f'Incorrect text: "{find_headling}"'
+    find_headling = driver.find_element(*NewInWomenLocators.new_in_women_title).text
+    assert find_headling == NewInWomenLocators.new_in_women_title_text, f'Incorrect text: "{find_headling}"'
 
 
 def test_hoodies_visible(driver):
     driver.get(NewPage.URL)
-    find_hoodies = driver.find_element(*NewInWomenLocators.hoodies_title)
-    assert find_hoodies.text == NewInWomenLocators.hoodies_text, f'Incorrect text: "{find_hoodies}"'
+    find_hoodies = driver.find_element(*NewInWomenLocators.hoodies_title).text
+    assert find_hoodies == NewInWomenLocators.hoodies_text, f'Incorrect text: "{find_hoodies}"'
 
 
 def test_jackets_visible(driver):
     driver.get(NewPage.URL)
-    find_jackets = driver.find_element(*NewInWomenLocators.jackets_title)
-    assert find_jackets.text == NewInWomenLocators.jackets_text, f'Incorrect text: "{find_jackets}"'
+    find_jackets = driver.find_element(*NewInWomenLocators.jackets_title).text
+    assert find_jackets == NewInWomenLocators.jackets_text, f'Incorrect text: "{find_jackets}"'
 
 
 def test_tees_visible(driver):
     driver.get(NewPage.URL)
-    find_tees = driver.find_element(*NewInWomenLocators.tees_title)
-    assert find_tees.text == NewInWomenLocators.tees_text, f'Incorrect text: "{find_tees}"'
+    find_tees = driver.find_element(*NewInWomenLocators.tees_title).text
+    assert find_tees == NewInWomenLocators.tees_text, f'Incorrect text: "{find_tees}"'
 
 
 def test_bra_visible(driver):
     driver.get(NewPage.URL)
-    find_bra = driver.find_element(*NewInWomenLocators.bra_title)
-    assert find_bra.text == NewInWomenLocators.bra_text, f'Incorrect text: "{find_bra}"'
+    find_bra = driver.find_element(*NewInWomenLocators.bra_title).text
+    assert find_bra == NewInWomenLocators.bra_text, f'Incorrect text: "{find_bra}"'
 
 
 def test_pants_visible(driver):
     driver.get(NewPage.URL)
-    find_pants = driver.find_element(*NewInWomenLocators.pants_title)
-    assert find_pants.text == NewInWomenLocators.pants_text, f'Incorrect text: "{find_pants}"'
+    find_pants = driver.find_element(*NewInWomenLocators.pants_title).text
+    assert find_pants == NewInWomenLocators.pants_text, f'Incorrect text: "{find_pants}"'
 
 
 def test_shorts_visible(driver):
     driver.get(NewPage.URL)
-    find_shorts = driver.find_element(*NewInWomenLocators.shorts_title)
-    assert find_shorts.text == NewInWomenLocators.shorts_text, f'Incorrect text: "{find_shorts}"'
+    find_shorts = driver.find_element(*NewInWomenLocators.shorts_title).text
+    assert find_shorts == NewInWomenLocators.shorts_text, f'Incorrect text: "{find_shorts}"'
