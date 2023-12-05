19aaee8d584463e807f380eeb97a065a1b885c92
evg-QA <freecadelka@yahoo.com>
2023-12-05 17:47:28 +0300

    TC_012.007.002 | added: parametrize, code refactoring

--- tests/privacy_and_cookie_policy_page/test_privacy_and_cookie_policy.py
+++ tests/privacy_and_cookie_policy_page/test_privacy_and_cookie_policy.py
@@ -14,13 +14,16 @@ def test_text_block_font_family_titled_your_choices_regarding_use_of_the_informa
     font_family = page.is_visible(locator=PrivacyCookiePolicyPageLocators.YOUR_CHOICES_REGARDING_USE_OF_THE_INFORMATION_WE_COLLECT_CONTENT_LOCATOR).value_of_css_property('font-family')
     assert font_family == PrivacyCookiePolicyFonts.TEXT_FONT_FAMILY
 
-def test_text_block_header_font_size_titled_your_choices_regarding_use_of_the_information_we_collect(driver):
+@pytest.mark.parametrize('locator, expected_font_size', [
+    (PrivacyCookiePolicyPageLocators.YOUR_CHOICES_REGARDING_USE_OF_THE_INFORMATION_WE_COLLECT_HEADER_LOCATOR, PrivacyCookiePolicyFonts.HEADER_TEXT_FONT_SIZE)])
+
+def test_text_block_header_font_size_titled_your_choices_regarding_use_of_the_information_we_collect(driver, locator, expected_font_size):
     """TC_012.007.002 | Footer > "Privacy and Cookie Policy" > Content >
      The Font-size of the title 'Your Choices Regarding Use Of The Information We Collect'"""
     page = BasePage(driver, url=PRIVACY_AND_COOKIE_POLICY_PAGE)
     page.open()
-    font_size = page.is_visible(locator=PrivacyCookiePolicyPageLocators.YOUR_CHOICES_REGARDING_USE_OF_THE_INFORMATION_WE_COLLECT_HEADER_LOCATOR).value_of_css_property('font-size')
-    assert font_size == PrivacyCookiePolicyFonts.HEADER_TEXT_FONT_SIZE
+    font_size = page.is_visible(locator).value_of_css_property('font-size')
+    assert font_size == expected_font_size
 
 def test_text_block_font_size_titled_your_choices_regarding_use_of_the_information_we_collect(driver):
     """TC_012.007.003 | Footer > "Privacy and Cookie Policy" > Content >
