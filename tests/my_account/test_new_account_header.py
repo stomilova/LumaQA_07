67ce6543c8e9775c622dd899b1b9ea4ab16d8d24
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2023-12-05 20:53:43 +0300

    [+] wait and driver fixture example fix (#258)

--- tests/my_account/test_new_account_header.py
+++ tests/my_account/test_new_account_header.py
@@ -1,10 +1,15 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait as wait
 
-driver = webdriver.Chrome()
+from base.seleniumbase import BasePage
 
-def test_new_account_header():
+
+# driver = webdriver.Chrome()
+
+def test_new_account_header(driver):
     """TC__004.003.009 | Authorization > check page title"""
     driver.get('https://magento.softwaretestingboard.com/customer/account/create/')
+    wait(driver, BasePage.TIMEOUT).until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'span[data-ui-id="page-title-wrapper"]')))
     header_text = driver.find_element(By.CSS_SELECTOR, 'span[data-ui-id="page-title-wrapper"]').text
     assert header_text == 'Create New Customer Account'
--- tests/wish_list_page_test/test_wish_list_page.py
+++ tests/wish_list_page_test/test_wish_list_page.py
@@ -1,11 +1,14 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait as wait
+
+from base.seleniumbase import BasePage
 from locators.wish_list_locators import WishListLocators
 
 
-class TestWishList():
+class TestWishList:
 
     def test_visibility_items_left_corner(self, driver, open_main_page, sign_in, add_items_to_wish_list):
+        wait(driver, BasePage.TIMEOUT).until(EC.visibility_of_element_located(WishListLocators.WISH_LIST_SIDEBAR_ITEMS))
         wish_list_elements = driver.find_elements(*WishListLocators.WISH_LIST_SIDEBAR_ITEMS)
         wish_list = [element.text for element in wish_list_elements]
         assert len(wish_list) >= 1, "Items not found"
-
-
