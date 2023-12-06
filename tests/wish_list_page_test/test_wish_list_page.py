8bbeed773b764884243d53e5a3e15c68129f40b7
Ekaterina Myshov <79929359+KatyaMy@users.noreply.github.com>
2023-12-05 23:58:24 -0500

    fixed tc (#262)

--- tests/wish_list_page_test/test_wish_list_page.py
+++ tests/wish_list_page_test/test_wish_list_page.py
@@ -1,6 +1,5 @@
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait as wait
-
 from base.seleniumbase import BasePage
 from locators.wish_list_locators import WishListLocators
 from pages.wish_list_page import WishListPage
@@ -14,7 +13,7 @@ class TestWishList:
         wish_list = [element.text for element in wish_list_elements]
         assert len(wish_list) >= 1, "Items not found"
 
-   def test_share_wish_list_button_is_working(self, driver, open_main_page, sign_in, add_items_to_wish_list):
+    def test_share_wish_list_button_is_working(self, driver, open_main_page, sign_in, add_items_to_wish_list):
         page = WishListPage(driver, url="https://magento.softwaretestingboard.com/wishlist/")
         page.click_share_wish_list_button()
         page.fill_email_field("not@gmail.com")
