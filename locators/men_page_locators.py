935330567152fbb09b23227c842e28d57aaf06da
Ekaterina Myshov <79929359+KatyaMy@users.noreply.github.com>
2023-12-07 20:47:13 -0500

    fixed tests (#275)

--- locators/men_page_locators.py
+++ locators/men_page_locators.py
@@ -9,9 +9,6 @@ class MenPageLocators:
     BOTTOMS_CATEGORY_LINK = (By.XPATH, "//*[@id='narrow-by-list2']/dd/ol/li[2]/a")
     SIDE_BAR_JACKETS = (By.XPATH, "//a[@id='ui-id-19']//span[contains(text(),'Jackets')]")
     MEN_TOPS_GRID = (By.XPATH, "//div[2]/div[1]/strong[2]")
-    MEN_TOPS_LIST = (By.CSS_SELECTOR, "#mode-list")
-    OLD_VIEW = (By.CLASS_NAME, "//*[@class='products wrapper grid products-grid']")
-    NEW_VIEW = (By.CLASS_NAME, "//*[@class='products wrapper list products-list']")
 
 
 class MenCategoryPageLocators:
