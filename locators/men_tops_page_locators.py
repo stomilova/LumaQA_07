4331434b20b3641286526f38770c819ffaa3ef35
Alena <132595359+alenabaska@users.noreply.github.com>
2023-12-08 17:32:27 +0700

    add test_men_tops_product_sorting suite (#270)
    * add test_men_tops_product_sorting suite
    
    * [+] waits
    [-] sleeps
    
    ---------
    
    Co-authored-by: aqaestuser <144237758+aqaestuser@users.noreply.github.com>

--- locators/men_tops_page_locators.py
+++ locators/men_tops_page_locators.py
@@ -4,3 +4,9 @@ from selenium.webdriver.common.by import By
 class MenTopsPageLocators:
     TOP_MEN_PRODUCT_FOTO = (By.CSS_SELECTOR, "a[class='product photo product-item-photo'] img[alt='Cassius Sparring Tank']")
     TOP_MEN_PRODUCT_TITLE = (By.XPATH, "//div[@class = 'product details product-item-details']//a[contains(text(), 'Cassius')]")
+    TOP_MEN_SORTER = (By.CSS_SELECTOR, '.toolbar-products:nth-child(3) .sorter-options')
+    TOP_MEN_PRODUCT_ITEMS_NAME = (By.CSS_SELECTOR, '.product-items .product-item-link')
+    TOP_MEN_ARROW = (By.CSS_SELECTOR, ".toolbar-products:nth-child(3) .sort-asc")
+    TOP_MEN_PRODUCT_ITEMS_PRICE = (By.CSS_SELECTOR, ".price-wrapper .price")
+    TOP_MEN_LIST_MODE = (By.CSS_SELECTOR, ".toolbar-products:nth-child(3) .mode-list")
+    # PAGE_NAME = (By.TAG_NAME, "h1")
--- pages/men_category_page/men_tops_page.py
+++ pages/men_category_page/men_tops_page.py
@@ -3,6 +3,7 @@ from locators.men_tops_page_locators import MenTopsPageLocators
 from locators.men_page_locators import MenPageLocators, MenCategoryPageLocators
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support.select import Select
 
 
 class MenTops(BasePage):
@@ -35,6 +36,30 @@ class MenTops(BasePage):
     def click_image(self, position):
         image = self.is_clickable(MenCategoryPageLocators.get_product_image(position))
         image.click()
+
+    def select_sorter(self, name):
+        self.is_clickable(MenTopsPageLocators.TOP_MEN_SORTER)
+        select_element = self.driver.find_element(*MenTopsPageLocators.TOP_MEN_SORTER)
+        select = Select(select_element)
+        select.select_by_visible_text(name)
+
+    def get_product_names(self):
+        self.is_visible_all_elements(MenTopsPageLocators.TOP_MEN_PRODUCT_ITEMS_NAME)
+        return self.driver.find_elements(*MenTopsPageLocators.TOP_MEN_PRODUCT_ITEMS_NAME)
+
+    def get_price(self):
+        self.is_visible_all_elements(MenTopsPageLocators.TOP_MEN_PRODUCT_ITEMS_PRICE)
+        return self.driver.find_elements(*MenTopsPageLocators.TOP_MEN_PRODUCT_ITEMS_PRICE)
+
+    def click_arrow(self):
+        self.is_clickable(MenTopsPageLocators.TOP_MEN_ARROW)
+        arrow = self.driver.find_element(*MenTopsPageLocators.TOP_MEN_ARROW)
+        arrow.click()
+
+    def click_list_mode(self):
+        self.is_visible(MenTopsPageLocators.TOP_MEN_LIST_MODE)
+        list_mode = self.driver.find_element(*MenTopsPageLocators.TOP_MEN_LIST_MODE)
+        list_mode.click()
 
     def mode_list_is_visible(self):
         return self.is_visible(MenCategoryPageLocators.LIST_MODE)
