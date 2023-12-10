df4b2757ae7504cefaf6d7930d61522b5f797a2a
Ekaterina Myshov <79929359+KatyaMy@users.noreply.github.com>
2023-12-10 18:50:35 -0500

    fixed locators after adding new feature to the website (#282)

--- locators/men_tops_page_locators.py
+++ locators/men_tops_page_locators.py
@@ -2,15 +2,16 @@ from selenium.webdriver.common.by import By
 
 
 class MenTopsPageLocators:
-    TOP_MEN_PRODUCT_FOTO = (By.CSS_SELECTOR, "a[class='product photo product-item-photo'] img[alt='Cassius Sparring Tank']")
-    TOP_MEN_PRODUCT_TITLE = (By.XPATH, "//div[@class = 'product details product-item-details']//a[contains(text(), 'Cassius')]")
-    TOP_MEN_SORTER = (By.CSS_SELECTOR, '.toolbar-products:nth-child(3) .sorter-options')
+    TOP_MEN_PRODUCT_FOTO = (By.CSS_SELECTOR,
+                            "a[class='product photo product-item-photo'] img[alt='Cassius Sparring Tank']")
+    TOP_MEN_PRODUCT_TITLE = (By.XPATH,
+                             "//div[@class = 'product details product-item-details']//a[contains(text(), 'Cassius')]")
+    TOP_MEN_SORTER = (By.CSS_SELECTOR, '.toolbar-products:nth-child(4) .sorter-options')
     TOP_MEN_PRODUCT_ITEMS_NAME = (By.CSS_SELECTOR, '.product-items .product-item-link')
-    TOP_MEN_ARROW = (By.CSS_SELECTOR, ".toolbar-products:nth-child(3) .sort-asc")
+    TOP_MEN_ARROW = (By.CSS_SELECTOR, ".toolbar-products:nth-child(4) .sort-asc")
     TOP_MEN_PRODUCT_ITEMS_PRICE = (By.CSS_SELECTOR, ".price-wrapper .price")
-    TOP_MEN_LIST_MODE = (By.CSS_SELECTOR, ".toolbar-products:nth-child(3) .mode-list")
-    # PAGE_NAME = (By.TAG_NAME, "h1")
-    SHOPING_OPTIONS_MENU=(By.CSS_SELECTOR,'#narrow-by-list div.filter-options-title')
+    TOP_MEN_LIST_MODE = (By.CSS_SELECTOR, ".toolbar-products:nth-child(4) .mode-list")
+    SHOPING_OPTIONS_MENU = (By.CSS_SELECTOR, '#narrow-by-list div.filter-options-title')
 
     @staticmethod
     def get_product_trough_foto(position: int):
